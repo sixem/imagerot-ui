e83a0dc7fd1bc54a960299ad6fcc579a359ea00d
emy <emy@five.sh>
2025-08-07 05:27:24 +0200

    chore: add automatic opening of devtools

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,14 +1,24 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{self, Manager};
+
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                if let Some(first) = app.webview_windows().values().next() {
+                    first.open_devtools();
+                }
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
