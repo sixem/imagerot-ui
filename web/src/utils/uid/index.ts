f3a286f8e139e299c8ed0b4c9e9a2deaf0f05e8e
emy <emy@five.sh>
2025-08-14 14:55:25 +0200

    refactor: improve uid function

--- web/src/utils/uid/index.ts
+++ web/src/utils/uid/index.ts
@@ -1,8 +1,11 @@
-export const uid = (() => {
-    let c = 0;
+let counter = 0;
 
-    return () =>
+export const uid = () => {
+    const base =
         (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function')
-            ? crypto.randomUUID()
-            : `${Date.now().toString(36)}-${(c++).toString(36)}`;
-})();
+            ? crypto.randomUUID() : Date.now().toString(36); // Deterministic fallback
+
+    console.log(`${base}:${(counter++).toString(36)}`);
+
+    return `${base}:${(counter++).toString(36)}`;
+};
