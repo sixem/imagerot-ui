77a1470c254879f6653a506e88946406f6a76096
emy <emy@five.sh>
2025-08-14 15:16:05 +0200

    fix: make uid() more filename friendly

--- web/src/utils/uid/index.ts
+++ web/src/utils/uid/index.ts
@@ -5,7 +5,5 @@ export const uid = () => {
         (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function')
             ? crypto.randomUUID() : Date.now().toString(36); // Deterministic fallback
 
-    console.log(`${base}:${(counter++).toString(36)}`);
-
-    return `${base}:${(counter++).toString(36)}`;
+    return `${base}-${(counter++).toString(36)}`;
 };
